9dc7e3d1d3e573fe6b33773367e33ac2265a09ca
Ameya Thakur <ameyat@codeaurora.org>
2016-09-26 12:07:36 -0700

    minerva: Generate non-hlos image mount points for A/B builds
    On builds where A/B support is enabled the system image acts as the
    rootfs. In this case we can no longer create the non-hlos image mount
    points at run time as we used to. We now create them as part of the
    build itself.
    
    Change-Id: I30d98f7c50fca3ce7117b9b8d87150655bb5b440

--- Android.mk
+++ Android.mk
@@ -12,6 +12,23 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
+# A/B builds require us to create the mount points at compile time.
+# Just creating it for all cases since it does not hurt.
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
+$(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
+
+BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
+$(BT_FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/bt_firmware
+
+DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
+$(DSP_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(DSP_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
+
 # Symlinks
 EGL_LIBRARIES := \
     libEGL_adreno.so \
@@ -94,6 +111,9 @@ $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
 
 ALL_DEFAULT_INSTALLED_MODULES += \
+    $(FIRMWARE_MOUNT_POINT) \
+    $(BT_FIRMWARE_MOUNT_POINT) \
+    $(DSP_MOUNT_POINT) \
     $(EGL_32_SYMLINKS) \
     $(EGL_64_SYMLINKS) \
     $(ACDBDATA_SYMLINKS) \
