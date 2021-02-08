5e7c3aeb0ed23e89931fadcdb1592168bbec1b1c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 14:52:53 +0100

    minerva: Inherit qcom vendor sepolicies
    Change-Id: I6e1fb3a236a13428f84e912e148a59a5476c91f7

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -90,3 +90,6 @@ TARGET_BOARD_PLATFORM := bengal
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# Sepolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
