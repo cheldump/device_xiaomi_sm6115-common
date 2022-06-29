3c573a4e3899fc2f24a235fe76e8f7634b4e1c6c
Omkar Chandorkar <gotenksIN@aospa.co>
2022-06-29 13:09:34 +0900

    minerva: Copy media_profiles to vendor/etc as well
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Change-Id: Id052bc1ed0a7dd959fa19d0373a9c5bf2b789330

--- media/media.mk
+++ media/media.mk
@@ -25,7 +25,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
