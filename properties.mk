08ba1ca95c8247fcd237bcc693ca0ec559c667d5
LuK1337 <priv.luk@gmail.com>
2022-09-02 14:14:28 +0200

    minerva: Pull appropriate USB props from vendor_product.mk
    Change-Id: I93b25c5d0989c83d1c8d7b7946a083771febdddd

--- properties.mk
+++ properties.mk
@@ -420,7 +420,13 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # USB
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.usb.diag.func.name=diag \
-    vendor.usb.use_ffs_mtp=0
+    vendor.usb.dpl.inst.name=dpl \
+    vendor.usb.qdss.inst.name=qdss \
+    vendor.usb.rmnet.func.name=gsi \
+    vendor.usb.rmnet.inst.name=rmnet \
+    vendor.usb.rndis.func.name=gsi \
+    vendor.usb.use_ffs_mtp=0 \
+    vendor.usb.use_gadget_hal=0
 
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.oem.otg_support=true
