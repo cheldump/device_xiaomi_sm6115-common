f0511dad8e684befa00a545ca3648fb5faf9f9ae
Alessio Balsini <balsini@google.com>
2023-03-14 20:37:25 +0800

    minerva: Use FUSE passthrough mode by default
    Enable the persist.sys.fuse.passthrough.enable flag in the device
    system configuration to enable the FUSE passthrough feature.
    This feature has been enabled on Cuttlefish devices for about two months
    and no issues have been detected yet.
    
    Bug: 168023149
    Test: 'adb shell getprop | grep persist.sys.fuse.passthrough.enable',
        ScopedStorageTest
    Change-Id: I874e2e2bd143513c32aedf27b824baccc5fcc478

--- properties.mk
+++ properties.mk
@@ -301,6 +301,10 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_VENDOR_PROPERTIES += \
     ro.hardware.fp.sideCap=true
 
+# FUSE Passthrough
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # Gatekeeper
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.gatekeeper.disable_spu=true
