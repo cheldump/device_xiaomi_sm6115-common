4e7b158cee13492ff3093aa05fbb5fe11934210e
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-06-28 16:30:56 +0700

    minerva: Define kernel flags
    Change-Id: I96c98ad19e43fbcad9ea6bcd0578e30f0d5df039

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -24,3 +24,30 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 TARGET_NO_BOOTLOADER := true
+
+# DTB
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_geni_serial,0x4a90000 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048 \
+    video=vfb:640x400,bpp=32,memsize=3072000
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image
+
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+TARGET_KERNEL_SOURCE := kernel/xiaomi/minerva
+TARGET_KERNEL_CONFIG := vendor/bengal-perf_defconfig
