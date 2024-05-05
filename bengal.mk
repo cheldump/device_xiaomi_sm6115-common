379ffbd693df4d0745387e0c380b2759393d4a82
HeroBuxx <me@herobuxx.me>
2024-05-05 20:24:32 +0000

    sm6115-common: Build libvendor.goodix.hardware.biometrics.fingerprint@2.1
    From hardware/xiaomi
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- bengal.mk
+++ bengal.mk
@@ -481,6 +481,9 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
